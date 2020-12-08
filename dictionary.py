19855a891fa2095fd5845d2ac2bdc6ecd3240e06
Александр <61043951+dvaxela@users.noreply.github.com>
2020-12-08 20:23:38 +0300

    Delete dictionary.py

--- dictionary.py
+++
@@ -1,8 +0,0 @@
-monitor = {'city': 'Москва', 'temperature': 12}
-print(monitor['city'])
-monitor['temperature'] -= 5
-print(monitor)
-monitor.get('country', 'Россия')
-monitor['date'] = '28/04/20'
-print(len(monitor))
-print(monitor)
